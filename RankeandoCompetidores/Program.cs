59e9f539e72f54a40772ad253aca9943a2f424ca
Flakes <ed.afram@gmail.com>
2020-10-26 23:20:30 -0300

    renomeação de variaveis

--- RankeandoCompetidores/Program.cs
+++ RankeandoCompetidores/Program.cs
@@ -12,50 +12,49 @@ namespace RankeandoCompetidores
          *  Artigos de referência: 
          *  https://docs.microsoft.com/pt-br/dotnet/machine-learning/how-to-guides/matchup-app-infer-net
          *  https://docs.microsoft.com/pt-br/archive/msdn-magazine/2019/february/test-run-rating-competitors-using-infer-net
-         *  
          */
         static void Main(string[] args)
         {
-            var nomesDosTimes = new string[] { "Angels", "Bruins", "Comets", "Demons", "Eagles", "Flyers" };
+            var nomesDosEquipes = new string[] { "Angels", "Bruins", "Comets", "Demons", "Eagles", "Flyers" };
 
-            var idsDosTimesVencedores = new int[] { 0, 2, 1, 0, 1, 3, 0, 2, 4 };
-            var idsDosTimesPerdedores = new int[] { 1, 3, 2, 4, 3, 5, 5, 4, 5 };
+            var IDsDasEquipesVencedoras = new int[] { 0, 2, 1, 0, 1, 3, 0, 2, 4 };
+            var IDsDasEquipesPerdedoras = new int[] { 1, 3, 2, 4, 3, 5, 5, 4, 5 };
 
-            var rangeDaQuantidadeDeTimes = new Range(nomesDosTimes.Length);
-            var rangeDaquantidadeDeJogos = new Range(idsDosTimesVencedores.Length);
+            var quantidadeDeEquipes = new Range(nomesDosEquipes.Length);
+            var quantidadeDePartidas = new Range(IDsDasEquipesVencedoras.Length);
 
 
-            var habilidades = Variable.Array<double>(rangeDaQuantidadeDeTimes);
-            habilidades[rangeDaQuantidadeDeTimes] = Variable.GaussianFromMeanAndVariance(6, 9).ForEach(rangeDaQuantidadeDeTimes);
+            var habilidades = Variable.Array<double>(quantidadeDeEquipes);
+            habilidades[quantidadeDeEquipes] = Variable.GaussianFromMeanAndVariance(6, 9).ForEach(quantidadeDeEquipes);
 
-            var vencedores = Variable.Array<int>(rangeDaquantidadeDeJogos);
-            var perdedores = Variable.Array<int>(rangeDaquantidadeDeJogos);
+            var vencedores = Variable.Array<int>(quantidadeDePartidas);
+            var perdedores = Variable.Array<int>(quantidadeDePartidas);
 
-            vencedores.ObservedValue = idsDosTimesVencedores;
-            perdedores.ObservedValue = idsDosTimesPerdedores;
+            vencedores.ObservedValue = IDsDasEquipesVencedoras;
+            perdedores.ObservedValue = IDsDasEquipesPerdedoras;
 
-            using (Variable.ForEach(rangeDaquantidadeDeJogos))
+            using (Variable.ForEach(quantidadeDePartidas))
             {
-                var performanceDoVencedor = Variable.GaussianFromMeanAndVariance(habilidades[vencedores[rangeDaquantidadeDeJogos]], 1);
-                var performanceDoPerdedor = Variable.GaussianFromMeanAndVariance(habilidades[perdedores[rangeDaquantidadeDeJogos]], 1);
+                var performanceDoVencedor = Variable.GaussianFromMeanAndVariance(habilidades[vencedores[quantidadeDePartidas]], 1);
+                var performanceDoPerdedor = Variable.GaussianFromMeanAndVariance(habilidades[perdedores[quantidadeDePartidas]], 1);
 
                 Variable.ConstrainTrue(performanceDoVencedor > performanceDoPerdedor);
             }
 
-            var mecanismoDeInferência = new InferenceEngine 
+            var mecanismoDeInferência = new InferenceEngine
             {
                 Algorithm = new ExpectationPropagation(),
-                NumberOfIterations = 100,
-                ShowProgress = true,
+                NumberOfIterations = 50,
+                ShowProgress = false,
             };
 
             var habilidadesInferidas = mecanismoDeInferência.Infer<Gaussian[]>(habilidades);
 
-            for (int i = 0; i < nomesDosTimes.Length; ++i)
+            for (int i = 0; i < nomesDosEquipes.Length; ++i)
             {
                 var habilidade = habilidadesInferidas[i].GetMean();
-                
-                Console.WriteLine($"{nomesDosTimes[i]}: {habilidade:F2}");
+
+                Console.WriteLine($"{nomesDosEquipes[i]}: {habilidade:F2}");
             }
         }
     }
